ff38b248c4918de811ff95b19dd0b728b5f8b96c
neme-sis <ghoshsayan894@gmail.com>
2022-10-07 00:05:04 +0530

    ascii code integration

--- randomPassword.js
+++ randomPassword.js
@@ -1,10 +1,16 @@
 var gen = document.getElementById('generate')
         const random = (noOfdigits) => {
             var result = "";
-            var f = "QWERTYUIOPLKJHGFDSZXCVABNMqazxswedcvfrtgbnhyujmkiolp0123456789";
-            var fLength = f.length;
+            // var f = "QWERTYUIOPLKJHGFDSZXCVABNMqazxswedcvfrtgbnhyujmkiolp0123456789";
+            // var fLength = f.length;
             for (i = 0; i < noOfdigits; i++) {
-                result = result + f.charAt(Math.floor(Math.random() * fLength));
+                // result = result + f.charAt(Math.floor(Math.random() * fLength));
+                let tempCode;
+                tempCode = 48 + Math.floor(Math.random()*(122-48))
+                if((tempCode>90 && tempCode < 97) || (tempCode>57 && tempCode < 65))
+                tempCode+=7;
+
+                result += String.fromCharCode(tempCode)
             }
             gen.innerHTML = result;
         }
